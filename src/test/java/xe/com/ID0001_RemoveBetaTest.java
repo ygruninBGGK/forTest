3bdc1c919f1aef9090d57c77551861c02824abcf
Yuri Grunin2 <ygrunin@xe.com>
2017-02-03 11:00:31 -0500

    Remove the temp page data

--- src/test/java/xe/com/ID0001_RemoveBetaTest.java
+++ src/test/java/xe/com/ID0001_RemoveBetaTest.java
@@ -53,8 +53,9 @@ public class ID0001_RemoveBetaTest {
 	  driver.get(baseUrl+"tools.php");
 	  Assert.assertFalse(driver.getPageSource().contains("beta"));
 	 }
+  
   @Test
-  public void a_redirecttoXecomBetaNotFound() throws InterruptedException{
+  public void b_redirecttoXecomBetaNotFound() throws InterruptedException{
 	
 	  driver.get(baseUrl);
 	  driver.get(baseUrl+"beta");
@@ -67,22 +68,7 @@ public class ID0001_RemoveBetaTest {
 	  Assert.assertEquals(txt03, "This page doesn’t exist. Try double checking the address to ensure that it is spelled correctly. Otherwise, check out our homepage or other services listed below:");
   }
   
-  @Test
-  public void a_redirecttoXecomMigrationFaq() throws InterruptedException{
-	
-	  driver.get("http://devserver08.xe.com:5081/migration/faq.php");
-	  String text1=  home.faqList_UI.get(12).getText();
-	  Assert.assertEquals(text1,"What will happen with the XE Currency App after the migration?");
-	  Thread.sleep(2000);
-	  home.faqLink_UI.get(12).click();
-	  Thread.sleep(2000);
-	  String text2= home.moduleParagraph_UI.get(0).getText();
-	  Assert.assertEquals(text2,"The XE Currency App has always been entirely independent from our XE Trade service, and as such is not affected in any way by this migration.");
-	  String text3= home.moduleParagraph_UI.get(1).getText();
-	  Assert.assertEquals(text3,"Our intention is that in future we will be able to develop deeper links between our services to allow us to continually enhance the customer experience we provide.");
-	  String text4= home.moduleParagraph_UI.get(2).getText();
-	  Assert.assertEquals(text4,"After October 30th, many elements of the new XE Money Transfer service are mobile responsive and you will easily be able to place transactions on your mobile device.");
-	  }
+ 
 @AfterClass
 public void close() {
 driver.close();
