6dcc0dca098e71ec2fdd2d9f2e318c7eea82a8ad
nsindhu <nsindhu@xe.com>
2017-02-15 11:07:48 -0500

    Load Tools and boxes - Changes

--- src/main/java/xecom_UI/LoadToolsandServiceBoxes_UI.java
+++ src/main/java/xecom_UI/LoadToolsandServiceBoxes_UI.java
@@ -8,26 +8,42 @@ import org.openqa.selenium.support.FindBy;
 public class LoadToolsandServiceBoxes_UI {
 
 	// heading for ad box
-    @FindBy(css = ".service.index-apps.featuredProducts h2 a")
+       @FindBy(css = ".service.index-xetrade.featuredProducts h2 a")
 	   public WebElement heading_UI;
     
-    //paragraph
-    @FindBy(css = ".serviceContent p")
+    // list of bullets
+       @FindBy(css = ".serviceContent li")
+	   public List<WebElement> servicecontent_UI;
+       
+    // list of bullets
+       @FindBy(css = ".serviceContent p")
 	   public List<WebElement> paragraph_UI;
     
-    //List 
-    @FindBy(css = ".serviceContent li")
+    // List 
+       @FindBy(css = ".serviceContent li")
 	   public List<WebElement> options_UI;
     
-    // download button
-    @FindBy(css = ".modFtr a")
-	   public WebElement download_UI;
+    // XEMT link
+       @FindBy(css = ".modFtr a")
+	   public List<WebElement> transfer_UI;
+       
+    // XEMT link - output and chart page
+       @FindBy(css = ".fServicesLnkBttm")
+	   public List<WebElement> servicelink_UI;
     
     // heading for ad box in input page
-    @FindBy(css = ".featuredServicesLnkTop.servicesLinkTitle a")
+       @FindBy(css = ".featuredServicesLnkTop.servicesLinkTitle a")
 	   public List<WebElement> inputPage_UI;
     
-    //  download button
-    @FindBy(css = ".modFtr a")
+    // download button
+       @FindBy(css = ".modFtr a")
 	   public List<WebElement> pageDownload_UI;
+       
+    // heading for ad box - outputpage 
+       @FindBy(css = ".service.ucc_convert-xetrade.featuredProducts h2 a")
+	   public WebElement output_UI;
+       
+    // heading for ad box - chart page
+       @FindBy(css = ".service.freecharts-xetrade.featuredProducts h2 a")
+	   public WebElement chartpage_UI;
 }
--- src/test/java/xe/com/ID0006_LoadToolsandServiceBoxes.java
+++ src/test/java/xe/com/ID0006_LoadToolsandServiceBoxes.java
@@ -19,7 +19,6 @@ import utils.General;
 import xecom_UI.LoadToolsandServiceBoxes_UI;
 
 //C9256	Load tools and services boxes by Ajax 
-//	C11176	Replace the XE Trade Ad on the community page
 
 public class ID0006_LoadToolsandServiceBoxes {
 
@@ -44,57 +43,55 @@ public class ID0006_LoadToolsandServiceBoxes {
 		 general.openStartingPage(baseUrl, usedBrowser);
 		 String text1 =   load.heading_UI.getText();
 		 Assert.assertEquals(text1,"Send a cheap money transfer ▶");
-		 String text2 =   load.paragraph_UI.get(0).getText();
-		 Assert.assertEquals(text2,"Looking to send money abroad? Transferring money online is easy with XE. Our service works with your bank and it's free to sign up.");
-		 String text3 =   load.paragraph_UI.get(1).getText();
-		 Assert.assertEquals(text3,"Available for iPhone, iPad, Android, Blackberry, Windows, Firefox OS devices.");
-		 String text4 =   load.options_UI.get(0).getText();
-		 Assert.assertEquals(text4,"Access live exchange rates");
-		 String text5 =   load.options_UI.get(1).getText();
-		 Assert.assertEquals(text5,"Calculate prices");
-		 String text6 =   load.options_UI.get(2).getText();
-		 Assert.assertEquals(text6,"View historical rate charts");
+		 String text10 =   load.paragraph_UI.get(0).getText();
+		 Assert.assertEquals(text10,"Looking to send money abroad? Transferring money online is easy with XE. Our service works with your bank and it's free to sign up.");
+		 String text2 =   load.servicecontent_UI.get(0).getText();
+		 Assert.assertEquals(text2,"Free online quotes");
+		 String text3 =   load.servicecontent_UI.get(1).getText();
+		 Assert.assertEquals(text3,"Competitive exchange rates");
+		 String text4 =   load.servicecontent_UI.get(2).getText();
+		 Assert.assertEquals(text4,"No-fee money transfer options");
+		 String text5 =   load.servicecontent_UI.get(3).getText();
+		 Assert.assertEquals(text5,"Bid for your preferred rate");
+		 String text6 =   load.transfer_UI.get(0).getText();
+		 Assert.assertEquals(text6,"Transfer Money with XE ▶");
 		 //input page
 		 driver.get(baseUrl+"currencyconverter/");
-		 String text9 =   load.inputPage_UI.get(1).getText();
-		 Assert.assertEquals(text9,"Currency rates on-the-go ▶");
-		 String text10 =   load.paragraph_UI.get(2).getText();
-		 Assert.assertEquals(text10,"Download the FREE XE Currency App for smartphone or tablet to");
-		 String text11 =   load.paragraph_UI.get(3).getText();
-		 Assert.assertEquals(text11,"Available for iPhone, iPad, Android, Blackberry, Windows, Firefox OS devices.");
-		 String text12 =   load.options_UI.get(0).getText();
-		 Assert.assertEquals(text12,"Access live exchange rates");
-		 String text13 =   load.options_UI.get(1).getText();
-		 Assert.assertEquals(text13,"Calculate prices");
-		 String text14 =   load.options_UI.get(2).getText();
-		 Assert.assertEquals(text14,"View historical rate charts");
-		 String text15 =   load.pageDownload_UI.get(2).getText();
-		 Assert.assertEquals(text15,"Download now ▶");
+		 String text9 =   load.inputPage_UI.get(0).getText();
+		 Assert.assertEquals(text9,"Send a cheap money transfer ▶");
+		 String text13 =   load.paragraph_UI.get(0).getText();
+		 Assert.assertEquals(text13,"Whether you are looking for personal or business use, it's easy to send an international money transfer with XE.");
+		 String text11 =   load.paragraph_UI.get(1).getText();
+		 Assert.assertEquals(text11,"We offer free online quotes, competitive exchange rates, no-fee payment options, and more. Signing up for an online account is free and our service works with your bank account.");
+		 String text12 =   load.transfer_UI.get(1).getText();
+		 Assert.assertEquals(text12,"Transfer Money with XE ▶");
 		 //output Page
 		 driver.findElement(By.id("ucc_go_btn_svg")).click();
+		 String text14 =   load.output_UI.getText();
+		 Assert.assertEquals(text14,"Send a cheap money transfer ▶");
 		 Externalad();
-		 String txt7 =   load.pageDownload_UI.get(1).getText();
-		 Assert.assertEquals(txt7,"Download now ▶");
 		 //currency chart
 		 driver.get(baseUrl+"currencycharts");
+		 String text15 =   load.chartpage_UI.getText();
+		 Assert.assertEquals(text15,"Send a cheap money transfer ▶");
 		 Externalad();
-		 String txt8 =   load.pageDownload_UI.get(0).getText();
-		 Assert.assertEquals(txt8,"Download now ▶");	 
+		  
 	 }
 		 
 	     private void Externalad(){
-			 String text8 =   load.inputPage_UI.get(0).getText();
-			 Assert.assertEquals(text8,"Currency rates on-the-go ▶");
-			 String txt2 =   load.paragraph_UI.get(0).getText();
-			 Assert.assertEquals(txt2,"Download the FREE XE Currency App for smartphone or tablet to");
-			 String txt3 =   load.paragraph_UI.get(1).getText();
-			 Assert.assertEquals(txt3,"Available for iPhone, iPad, Android, Blackberry, Windows, Firefox OS devices.");
-			 String txt4 =   load.options_UI.get(0).getText();
-			 Assert.assertEquals(txt4,"Access live exchange rates");
-			 String txt5 =   load.options_UI.get(1).getText();
-			 Assert.assertEquals(txt5,"Calculate prices");
-			 String txt6 =   load.options_UI.get(2).getText();
-			 Assert.assertEquals(txt6,"View historical rate charts");
+	    	 
+			 String text5 =   load.paragraph_UI.get(0).getText();
+			 Assert.assertEquals(text5,"Looking to send money abroad? Transferring money online is easy with XE. Our service works with your bank and it's free to sign up.");
+	    	 String text2 =   load.servicecontent_UI.get(0).getText();
+			 Assert.assertEquals(text2,"Free online quotes");
+			 String text3 =   load.servicecontent_UI.get(1).getText();
+			 Assert.assertEquals(text3,"Competitive exchange rates");
+			 String text4 =   load.servicecontent_UI.get(2).getText();
+			 Assert.assertEquals(text4,"No-fee money transfer options");
+			 String text7 =   load.servicecontent_UI.get(3).getText();
+			 Assert.assertEquals(text7,"Bid for your preferred rate");
+			 String text6 =   load.servicelink_UI.get(0).getText();
+			 Assert.assertEquals(text6,"Transfer Money with XE ▶");
 		 }
 		 
 	 
